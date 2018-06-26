4a1841956b653c85c0a66ebf94398aa8810054a6
wslates <wesleyslates@gmail.com>
2018-06-26 13:44:57 -0700

    Added support for NR2003 Directory and Cancel Button
    Series view/view model now supports selection of NR2003 directory. Also added length checking in series view model so user cannot create constraint errors on the database end. Lastly, added support for cancel button in the series view/view model.

--- NR2K3Results_MVVM/ViewModel/SeriesViewModel.cs
+++ NR2K3Results_MVVM/ViewModel/SeriesViewModel.cs
@@ -26,13 +26,15 @@ namespace NR2K3Results_MVVM.ViewModel
         private String rosterFile;
         private String seriesLogo;
         private String sancLogo;
+        private String NR2k3Dir;
 
         public RelayCommand OpenRosterFileCommand { get; private set; }
         public RelayCommand LoadNewSeriesCommand { get; private set; }
         public RelayCommand LoadNewSancCommand { get; private set; }
         public RelayCommand<Window> SaveSeriesCommand { get; private set; }
-        public RelayCommand CancelCommand { get; private set; }
+        public RelayCommand<Window> CancelCommand { get; private set; }
         public RelayCommand OnCloseCommand { get; private set; }
+        public RelayCommand NR2K3RootCommand { get; private set; }
 
         public String Name
         {
@@ -41,8 +43,12 @@ namespace NR2K3Results_MVVM.ViewModel
                 return name;
             }
             set
-            {
-                name = value;
+            { 
+                if (value.Length>64)
+                {
+                    shortName = value.Substring(0, 64);
+                } else
+                    name = value;
                 RaisePropertyChanged();
             }
         }
@@ -55,7 +61,11 @@ namespace NR2K3Results_MVVM.ViewModel
             }
             set
             {
-                shortName = value;
+                if (value.Length>16)
+                {
+                    shortName = value.Substring(0, 16);
+                } else
+                    shortName = value;
                 RaisePropertyChanged();
             }
         }
@@ -99,6 +109,19 @@ namespace NR2K3Results_MVVM.ViewModel
             }
         }
 
+        public String NR2K3Dir
+        {
+            get
+            {
+                return NR2k3Dir;
+            }
+            set
+            {
+                NR2k3Dir = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public SeriesViewModel()
         {
             OpenRosterFileCommand = new RelayCommand(OpenRosterFileCommandAction);
@@ -106,7 +129,29 @@ namespace NR2K3Results_MVVM.ViewModel
             LoadNewSancCommand = new RelayCommand(LoadNewSancCommandAction);
             SaveSeriesCommand = new RelayCommand<Window>(SaveSeriesCommandAction);
             OnCloseCommand = new RelayCommand(OnCloseCommandAction);
+            NR2K3RootCommand = new RelayCommand(NR2K3RootCommandAction);
+            CancelCommand = new RelayCommand<Window>(CancelCommandAction);
             Messenger.Default.Register<Model.SendDataToSeriesView>(this, ReceiveSeriesData);
+        }
+
+        private void CancelCommandAction(Window obj)
+        {
+            obj.Close();
+        }
+
+        private void NR2K3RootCommandAction()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog
+            {
+                InitialDirectory = "C:\\",
+                IsFolderPicker = true
+            };
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok && !String.IsNullOrWhiteSpace(dialog.FileName))
+            {
+                NR2K3Dir = dialog.FileName;
+                
+            }
         }
 
         private void OnCloseCommandAction()
@@ -121,6 +166,7 @@ namespace NR2K3Results_MVVM.ViewModel
             {
 
                 selectedSeries = context.Series.Where(d => d.SeriesName.Equals(obj.series)).FirstOrDefault();
+                NR2K3Dir = selectedSeries?.NR2K3Dir;
                 Name = selectedSeries?.SeriesName;
                 ShortName = selectedSeries?.SeriesShort;
                 RosterFull = selectedSeries?.RosterFile;
@@ -198,7 +244,7 @@ namespace NR2K3Results_MVVM.ViewModel
 
         public void SaveSeriesCommandAction(Window window)
         {
-            if (String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(ShortName) || String.IsNullOrEmpty(RosterFull))
+            if (String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(ShortName) || String.IsNullOrEmpty(RosterFull) || String.IsNullOrEmpty(NR2k3Dir))
             {
                 MessageBox.Show("Please enter all required data!", "Error Saving Series!", MessageBoxButton.OK, MessageBoxImage.Error);
             } else
@@ -209,7 +255,8 @@ namespace NR2K3Results_MVVM.ViewModel
                     SeriesShort = ShortName,
                     RosterFile = RosterFull,
                     SeriesLogo = SeriesFull,
-                    SancLogo = SancFull
+                    SancLogo = SancFull,
+                    NR2K3Dir = NR2k3Dir
                 };
 
                 if (selectedSeries != null) { context.Series.Remove(selectedSeries); }
